62cde0f03ad314fb61140ec781a55e6ab68912a5
Lillie <aedl@itu.dk>
2017-03-26 00:43:24 +0100

    BLE service

---
+++ Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/BLEService.java
@@ -0,0 +1,123 @@
+package com.example.anders.flexsensor;
+
+import android.app.Service;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothProfile;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import java.util.UUID;
+
+/**
+ * Manages connection and operations on a GATT server
+ */
+
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class BLEService extends Service{
+    private static final String TAG = BLEService.class.getSimpleName();
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+
+    private BluetoothGatt bluetoothGatt;
+    private int connectionState = STATE_DISCONNECTED;
+
+    //GATT actions
+    public static final String ACTION_GATT_CONNECTED =
+            "com.example.anders.flexsensor.ACTION_GATT_CONNECTED";
+    public static final String ACTION_GATT_DISCONNECTED =
+            "com.example.anders.flexsensor.ACTION_GATT_DISCONNECTED";
+    public static final String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.anders.flexsensor.ACTION_GATT_SERVICES_DISCOVERED";
+    public static final String ACTION_DATA_AVAILABLE =
+            "com.example.anders.flexsensor.ACTION_DATA_AVAILABLE";
+    public static final String EXTRA_DATA =
+            "com.example.anders.flexsensor.EXTRA_DATA";
+
+    public final static UUID UUID_BATTERY_SERVICE = UUID.fromString("0x180F");
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            String intentAction;
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                intentAction = ACTION_GATT_CONNECTED;
+                connectionState = STATE_CONNECTED;
+                broadcastUpdate(intentAction);
+                Log.i(TAG, "Connected to GATT server.");
+                Log.i(TAG, "Attempting to start service discovery" +
+                        bluetoothGatt.discoverServices());
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                intentAction = ACTION_GATT_DISCONNECTED;
+                connectionState = STATE_CONNECTED;
+                Log.i(TAG, "Disconnected from GATT server.");
+                broadcastUpdate(intentAction);
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+            } else {
+                Log.w(TAG, "onServicesDiscovered received: " + status);
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+            }
+        }
+    };
+
+    private void broadcastUpdate(final String action) {
+        final Intent intent = new Intent(action);
+        sendBroadcast(intent);
+    }
+
+    private void broadcastUpdate(final String action,
+                                 final BluetoothGattCharacteristic characteristic) {
+        final Intent intent = new Intent(action);
+
+        if (UUID_BATTERY_SERVICE.equals(characteristic.getUuid())) {
+            int flag = characteristic.getProperties();
+            int format;
+            if ((flag & 0x01) != 0) { //TODO: Not sure about this
+                format = BluetoothGattCharacteristic.FORMAT_UINT16;
+                Log.d(TAG, "Battery service format UINT16.");
+            } else {
+                format = BluetoothGattCharacteristic.FORMAT_UINT8;
+                Log.d(TAG, "Battery service format UINT8.");
+            }
+            final int angle = characteristic.getIntValue(format, 1);
+            Log.d(TAG, String.format("Received angle: %d", angle));
+            intent.putExtra(EXTRA_DATA, String.valueOf(angle));
+        } else {
+            final byte[] data = characteristic.getValue();
+            if (data != null && data.length > 0) {
+                final StringBuilder stringBuilder = new StringBuilder(data.length);
+                for (byte byteChar : data) {
+                    stringBuilder.append(String.format("%02X ", byteChar));
+                }
+                intent.putExtra(EXTRA_DATA, new String(data) + "\n" +
+                        stringBuilder.toString());
+            }
+        }
+        sendBroadcast(intent);
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/FlexSensorManager.java
+++ Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/FlexSensorManager.java
@@ -1,6 +1,5 @@
 package com.example.anders.flexsensor;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 

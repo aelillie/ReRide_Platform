8bdb7a1aa3186d8254e117ced86a025b832f7ec6
Anders Edelbo Lillie <aedl@itu.dk>
2017-03-31 14:28:45 +0200

    Rework main

--- Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/MainActivity.java
+++ Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/MainActivity.java
@@ -1,124 +1,134 @@
 package com.example.anders.flexsensor;
 
+import com.example.anders.flexsensor.ble.BLEDeviceControl;
+import com.example.anders.flexsensor.ble.BLEDeviceScanner;
 import android.Manifest;
-import android.content.DialogInterface;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.anders.flexsensor.ble.BLEActivity;
 
-public class MainActivity extends AppCompatActivity implements FlexSensorManager.CallBack{
+public class MainActivity extends AppCompatActivity
+        implements FlexSensorManager.CallBack, BLEDeviceScanner.ScanResultCallback{
 
     public static final String TAG = MainActivity.class.getCanonicalName();
+    public static final int REQUEST_ENABLE_BT = 1; //Request code for BLE Intent
     private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
 
-    private TextView stateText;
-    private EditText angleText;
-    private Button updateButton;
-    private Button refresh;
-    private Button scanner;
-
     private FlexSensorManager flexSensorManager;
+    private BLEDeviceScanner bleDeviceScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        askForPermission();
+        getActionBar().setTitle(R.string.app_name);
+
+        checkForBLESupport();
+        askForLocationPermission();
         flexSensorManager = new FlexSensorManager(this);
-        stateText = (TextView) findViewById(R.id.stateText);
-        angleText = (EditText) findViewById(R.id.angle_text);
-        updateButton = (Button) findViewById(R.id.updateAngle);
-        updateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String angle = angleText.getText().toString();
-                flexSensorManager.update(angle);
-            }
-        });
-        refresh = (Button) findViewById(R.id.refresh);
-        refresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                flexSensorManager.getShadow();
-            }
-        });
-        scanner = (Button) findViewById(R.id.bleScanButton);
-        scanner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                scanForDevice();
-            }
-        });
+        bleDeviceScanner = new BLEDeviceScanner(this);
+
     }
 
-    private void askForPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Android M Permission check
-            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!bleDeviceScanner.bleIsEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        bleDeviceScanner.scanBLEDevice(true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void askForLocationPermission() {
+        // Prompt for permissions
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("This app needs location access");
-                builder.setMessage("Please grant location access so this app can detect beacons.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @RequiresApi(api = Build.VERSION_CODES.M)
-                    public void onDismiss(DialogInterface dialog) {
-                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                PERMISSION_REQUEST_FINE_LOCATION);
-                    }
-                });
-                builder.show();
+                Log.w("BleActivity", "Location access not granted!");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSION_REQUEST_FINE_LOCATION);
             }
         }
     }
 
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_FINE_LOCATION: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "coarse location permission granted");
-                } else {
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("Functionality limited");
-                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
-                    builder.setPositiveButton(android.R.string.ok, null);
-                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                        }
-                    });
-                    builder.show();
-                }
-            }
+    private void checkForBLESupport() {
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        if (bleDeviceScanner.isScanning()) {
+            menu.findItem(R.id.menu_scan).setVisible(false);
+            menu.findItem(R.id.menu_stop).setVisible(true);
+        } else {
+            menu.findItem(R.id.menu_scan).setVisible(true);
+            menu.findItem(R.id.menu_stop).setVisible(false);
+        }
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_scan:
+                bleDeviceScanner.scanBLEDevice(true);
+                break;
+            case R.id.menu_stop:
+                bleDeviceScanner.scanBLEDevice(false);
+                break;
+        }
+        return true;
+    }
+
 
     @Override
     public void updatedState(int newState) {
 //        angleText.setText(newState);
     }
 
-    private void scanForDevice() {
-        if (BLEActivity.isBLESupported(this)) {
-            Intent intent = new Intent(this, BLEActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "BLE not supported", Toast.LENGTH_LONG).show();
+    @Override
+    public void foundDevice(String deviceName, String deviceAddress) {
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        final Intent intent = new Intent(this, BLEDeviceControl.class);
+        intent.putExtra(BLEDeviceControl.EXTRAS_DEVICE_NAME, deviceName);
+        intent.putExtra(BLEDeviceControl.EXTRAS_DEVICE_ADDRESS, deviceAddress);
+        if (bleDeviceScanner.isScanning()) {
+            bleDeviceScanner.scanBLEDevice(false);
         }
+        startActivity(intent);
+    }
+
+    @Override
+    public void deviceNotFound() {
+        Toast.makeText(this, "No device found!", Toast.LENGTH_SHORT).show();
     }
 }

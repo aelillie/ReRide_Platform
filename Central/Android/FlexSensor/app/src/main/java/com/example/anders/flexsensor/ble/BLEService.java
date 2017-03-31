4149369280af2ba25ad3c7e6e9b1daec25cc3198
Anders Edelbo Lillie <aedl@itu.dk>
2017-03-31 11:07:39 +0200

    Change reading properties

--- Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/ble/BLEService.java
+++ Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/ble/BLEService.java
@@ -25,6 +25,8 @@ public class BLEService extends Service{
     private static final int STATE_DISCONNECTED = 0;
     private static final int STATE_CONNECTING = 1;
     private static final int STATE_CONNECTED = 2;
+    private static final UUID UUID_APPARENT_WIND_DIRECTION =
+            UUID.fromString(GattAttributes.APPARENT_WIND_DIRECTION);
 
     private final IBinder binder = new LocalBinder();
 
@@ -43,9 +45,6 @@ public class BLEService extends Service{
             "com.example.anders.flexsensor.ACTION_DATA_AVAILABLE";
     public static final String EXTRA_DATA =
             "com.example.anders.flexsensor.EXTRA_DATA";
-
-    public final static UUID UUID_BATTERY_LEVEL =
-            UUID.fromString(GattAttributes.BATTERY_LEVEL);
 
     private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
         /*
@@ -109,31 +108,31 @@ public class BLEService extends Service{
                                  final BluetoothGattCharacteristic characteristic) {
         final Intent intent = new Intent(action);
 
-        if (UUID_BATTERY_LEVEL.equals(characteristic.getUuid())) {
-            int flag = characteristic.getProperties();
-            int format;
-            if ((flag & 0x01) != 0) {
-                format = BluetoothGattCharacteristic.FORMAT_UINT16;
-                Log.d(TAG, "Battery service format UINT16.");
-            } else {
-                format = BluetoothGattCharacteristic.FORMAT_UINT8;
-                Log.d(TAG, "Battery service format UINT8.");
+        if (UUID_APPARENT_WIND_DIRECTION.equals(characteristic.getUuid())) {
+            try {
+                int format = BluetoothGattCharacteristic.FORMAT_UINT16;
+                final int windDir = characteristic.getIntValue(format, 0);
+                Log.d(TAG, String.format("Apparent wind direction: %d", windDir));
+                intent.putExtra(EXTRA_DATA, String.valueOf(windDir));
+            } catch (NullPointerException e) {
+                intent.putExtra(EXTRA_DATA, readUnknownData(characteristic));
             }
-            final int angle = characteristic.getIntValue(format, 1);
-            Log.d(TAG, String.format("Received angle: %d", angle));
-            intent.putExtra(EXTRA_DATA, String.valueOf(angle));
         } else {
-            final byte[] data = characteristic.getValue();
-            if (data != null && data.length > 0) {
-                final StringBuilder stringBuilder = new StringBuilder(data.length);
-                for (byte byteChar : data) {
-                    stringBuilder.append(String.format("%02X ", byteChar));
-                }
-                intent.putExtra(EXTRA_DATA, new String(data) + "\n" +
-                        stringBuilder.toString());
-            }
+            intent.putExtra(EXTRA_DATA, readUnknownData(characteristic));
         }
         sendBroadcast(intent);
+    }
+
+    private String readUnknownData(BluetoothGattCharacteristic characteristic) {
+        final byte[] data = characteristic.getValue();
+        String dataString = "";
+        if (data != null && data.length > 0) {
+            final StringBuilder stringBuilder = new StringBuilder(data.length);
+            for(byte byteChar : data)
+                stringBuilder.append(String.format("%02X ", byteChar));
+            dataString = new String(data) + "\n" + stringBuilder.toString();
+        }
+        return dataString;
     }
 
     public class LocalBinder extends Binder {

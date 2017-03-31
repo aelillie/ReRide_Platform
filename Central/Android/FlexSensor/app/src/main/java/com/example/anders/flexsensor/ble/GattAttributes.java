b5a8f16145e51e5ab3b3d791d4b0fb644a01dc64
Anders Edelbo Lillie <aedl@itu.dk>
2017-03-31 10:56:32 +0200

    Add new service

--- Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/ble/GattAttributes.java
+++ Central/Android/FlexSensor/app/src/main/java/com/example/anders/flexsensor/ble/GattAttributes.java
@@ -8,13 +8,19 @@ import java.util.UUID;
  */
 
 class GattAttributes {
-    private static final String baseBluetoothUuidPostfix = "0000-1000-8000-00805F9B34FB";
+    private static final String baseBluetoothUuidPostfix = "0000-1000-8000-00805f9b34fb";
     private static HashMap<String, String> attributes = new HashMap<>();
     static String BATTERY_LEVEL = uuidSStringFromUInt16("2A19");
+    static String APPARENT_WIND_DIRECTION = uuidSStringFromUInt16("2a73");
 
     static {
+        //Services
         attributes.put(uuidSStringFromUInt16("180F"), "Battery Service");
+        attributes.put(uuidSStringFromUInt16("181a"), "Environmental Sensing");
+
+        //Characteristics
         attributes.put(BATTERY_LEVEL, "Battery Level");
+        attributes.put(APPARENT_WIND_DIRECTION, "Apparent Wind Direction");
     }
 
     static String lookup(String uuid, String defaultName) {

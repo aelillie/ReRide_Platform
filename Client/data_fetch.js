e3650e841a8ae6155464af1e0613fa3cb1655c84
Anders Edelbo Lillie <aedl@itu.dk>
2017-04-19 14:41:53 +0200

    Trying to make a client front-end

---
+++ Client/data_fetch.js
@@ -0,0 +1,36 @@
+console.log('Loading function');
+
+var AWS = require('aws-sdk');
+var DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function(event, context, callback) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    let id = event.id;
+    let since = parseInt(event.since);
+
+    var startTime = new Date(
+        Date.now() - (since*60000) //min to ms
+        ).toTimeString();
+
+    var params = {
+        TableName: 'ReRide_DDB',
+        KeyConditionExpression: "ThingID = :i AND #T > :t",
+        ExpressionAttributeValues: {
+            ":i": id,
+            "#T": 'Time',
+            ":t": startTime
+        },
+        ConsistentRead: true
+    }
+
+    DynamoDB.query(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            callback(err, null);
+        } else {
+            console.log(data);           // successful response
+            callback(null, data);
+        }     
+    })
+};
